3a16d274fa07fe0c5e13ccdd63840e8fcdbf3e96
Viktor Boshkov <157938507+viktorboshkov@users.noreply.github.com>
2024-02-17 06:26:34 +0100

    Exercise 3
    With different approaches

---
+++ Структурно Програмирање/Вежби и Задачи/Аудиториски 5/Задача 3 FOR.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+
+   int x,  // Number of numbers that will be entered
+    number, // Number entered
+    rem_1 = 0, // Count of numbers with remainder 1
+    rem_2 = 0, // Count of numbers with remainder 2
+    div_3 = 0; // Count of numbers divisible by 3
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &x);
+
+    printf("Enter %d numbers:\n", x);
+    for(int i = 0; i < x; i++){
+        scanf("%d", &number);
+        if (number % 3 == 0) {
+            div_3++;
+        } else if (number % 3 == 1) {
+            rem_1++;
+        } else if (number % 3 == 2) {
+            rem_2++;
+        }
+    }
+
+    printf("Divisible by 3: %d\n", div_3);
+    printf("Reminder of 1: %d\n", rem_1);
+    printf("Reminder of 2: %d\n", rem_2);
+
+    return 0;
+}
