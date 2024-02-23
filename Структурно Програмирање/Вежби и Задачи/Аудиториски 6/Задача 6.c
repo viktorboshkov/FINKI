d74007463911a815cbfd6fddbd66c5eb632d5b2e
Viktor Boshkov <157938507+viktorboshkov@users.noreply.github.com>
2024-02-23 01:12:24 +0100

    Задача 6
    What i came up with at the moment

---
+++ Структурно Програмирање/Вежби и Задачи/Аудиториски 6/Задача 6.c
@@ -0,0 +1,36 @@
+//Задача 5
+//Да се напише програма за ротирање на елементите на една низа за едно место
+//во десно. На крај, да се испечати на екран ротираната низа. Елементите од
+//низата се читаат од стандарден влез.
+
+#include <stdio.h>
+#define MAX 100
+int main() {
+
+    int array[MAX];
+
+    int n,size = 0, m;
+    scanf("%d", &m);
+    for(int i = 0; i < MAX; i++){
+        if(scanf("%d", &n)) {
+            array[i] = n;
+            size++;
+        }else break;
+    }
+
+
+    for(int i = 0; i < m; i++){
+        int tmp;
+        tmp = array[size-1]; //save the last element
+        for(int j = size - 1; j > 0; j--){ // 1 2 3 4 5 - > 4 5 1 2 3
+            array[j] = array[j-1];
+        }
+        array[0] = tmp;
+
+    }
+
+    for(int i = 0; i < n; i++) {
+        printf("%d ", array[i]);
+    }
+    return 0;
+}
