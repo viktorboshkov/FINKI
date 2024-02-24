8c0e332a0506b49262e7444938b3fd1bb3ad79e4
Viktor Boshkov <157938507+viktorboshkov@users.noreply.github.com>
2024-02-24 06:26:36 +0100

    Create Задача 1 Матрици.c

---
+++ Структурно Програмирање/Вежби и Задачи/Аудиториски 6/Задача 1 Матрици.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define MAX 100
+
+int main() {
+    int MAT[MAX][MAX];
+    int n, m, entry, sumEven = 0, sumOdd = 0, difference;
+    printf("Enter size of Matrix: N: M: \n");
+    scanf("%d %d", &n, &m);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &entry);
+            MAT[i][j] = entry;
+            if (j % 2 != 0) sumOdd += MAT[i][j]; // Сума на непарните колони
+            if (i % 2 == 0) sumEven += MAT[i][j]; // Сума на парните редици
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("\n");
+        for (int j = 0; j < m; j++) {
+            printf("%3d ", MAT[i][j]);
+        }
+    }
+
+    // Пресметка на разликата
+    if (sumEven > sumOdd) difference = sumEven - sumOdd;
+    else difference = sumOdd - sumEven;
+
+    printf("\nEven sum: %d. Odd Sum:%d\n", sumEven, sumOdd);
+    printf("Difference: %d", difference);
+
+    return 0;
+}
