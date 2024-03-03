6dc83fbd2a6700436c5f967e70e8591fa9f426b0
Viktor Boshkov <157938507+viktorboshkov@users.noreply.github.com>
2024-03-03 06:02:36 +0100

    Create L1 - 5.c

---
+++ Структурно Програмирање/Вежби и Задачи/Лабораториски 1 - 6/L1 - 5.c
@@ -0,0 +1,39 @@
+// Да се напише програма во која во даден опсег на природни броеви [a,b],
+// ќе се најдат и отпечатат сите броеви чијшто збир на цифри
+// кои се деливи со 2 е поголем од 0 и е делив со 4.
+// На крај да се отпечати и бројот на вакви броеви.
+
+#include <stdio.h>
+
+int main(){
+   int a, b, number, digit, sumDigits, counter = 0, flag = 0;
+    scanf("%d %d", &a, &b);
+
+   for(int i = a; i <= b; i++){
+        number = i;
+        sumDigits = 0;
+
+       while (number){
+           digit = number % 10;
+           if (digit % 2 == 0 && digit % 3 != 0) {
+               if (number > 9) sumDigits += digit;
+               else sumDigits += number;
+               number /= 10;
+           } else {
+                flag = 0;
+               break;
+           }
+           flag++;
+       }
+        if(flag) {
+            if(sumDigits > 0 && sumDigits % 7 == 0){
+            printf("Vkupno: %d\n", i);
+            counter++;
+            }
+        }
+   }
+
+    printf("%d", counter);
+    return 0;
+
+}
