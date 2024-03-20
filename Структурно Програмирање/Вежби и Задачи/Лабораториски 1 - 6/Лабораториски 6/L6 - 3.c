e7456800ac2632d8d353172582fa15ff27b879f0
Viktor Boshkov <157938507+viktorboshkov@users.noreply.github.com>
2024-03-20 17:02:07 +0100

    Create L6 - 3.c

---
+++ Структурно Програмирање/Вежби и Задачи/Лабораториски 1 - 6/Лабораториски 6/L6 - 3.c
@@ -0,0 +1,31 @@
+// Од стандарден влез се чита позитивен број n кој ги дава димензиите на квадратна матрица од цели броеви,
+// која се чита во продолжение. Да се напише програма која за таа матрица(макс. 100 x 100)
+//cќе ја испечати должината на најдолгата строго растечка подниза,
+//доколку матрицата ја гледаме ред по ред.
+
+#include <stdio.h>
+int main (){
+    int n, count = 0,max = 0, postion = 0;
+    scanf("%d", &n);
+    int arrays[n][n];
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &arrays[i][j]);
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(arrays[i][j + 1] > arrays[i][j]) count++;
+                 if(count > max){
+                    max = count;
+                    postion = i + 1;
+                }
+            }
+        count = 0;
+        }
+    printf("%d", postion);
+
+    return 0;
+}
